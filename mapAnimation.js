837c79561bcd1f285e5624c6aaa68c8b0c9cc214
edllanos1 <78517244+edllanos1@users.noreply.github.com>
2021-04-25 00:05:53 -0400

    Update mapAnimation.js

--- mapAnimation.js
+++ mapAnimation.js
@@ -22,7 +22,7 @@ let map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11',
   center: [-71.104081, 42.365554],
-  zoom: 14,
+  zoom: 16,
 });
 
 // TODO: add a marker to the map
